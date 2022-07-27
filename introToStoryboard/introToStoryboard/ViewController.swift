ac1913c557c788135c6a748604fd1bf8ee7de5b1
victoriajan <110115781+victoriajan@users.noreply.github.com>
2022-07-27 11:18:21 -0400

    This is our first Xcode Project

---
+++ introToStoryboard/introToStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introToStoryboard
+//
+//  Created by scholar on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+    }
+
+
+}
+
