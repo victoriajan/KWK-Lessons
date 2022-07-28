e7df0b57f6ee1e54e0fd8bf1e2e920a951ab35e5
victoriajan <110115781+victoriajan@users.noreply.github.com>
2022-07-28 09:59:02 -0400

    Finished intro to segues

---
+++ introToSegues/introToSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introToSegues
+//
+//  Created by scholar on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
